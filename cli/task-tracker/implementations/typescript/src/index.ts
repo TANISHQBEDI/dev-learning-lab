6bd853b3e14d1298b23532e916c515b4d56b4b22
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2025-12-20 03:16:11 +1100

    chore(cli-task-tracker-typescript): Add a main class to start the application

--- cli/task-tracker/implementations/typescript/src/index.ts
+++ cli/task-tracker/implementations/typescript/src/index.ts
@@ -1,3 +1,19 @@
 import * as process from "node:process";
 
-console.log(process.argv)
+
+class TaskTracker{
+
+    constructor() {}
+
+    main(){
+        console.log("Task Tracker Application Started");
+    }
+
+    static bootstrap(args: string[]){
+        const app = new TaskTracker();
+        app.main();
+    }
+
+}
+
+TaskTracker.bootstrap(process.argv.splice(2));
